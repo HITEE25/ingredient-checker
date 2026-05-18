0008bd26805e2faa0f5f4e3d091c7d91478d5e41
HITEE25 <hitee0025@gmail.com>
2026-05-18 11:59:36 +0530

    Use environment variable for session secret

--- middleware/app.js
+++ middleware/app.js
@@ -1,7 +1,7 @@
 // app.js - Add session middleware
 const session = require('express-session');
 app.use(session({
-  secret: 'hitEE#1725',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: { maxAge: 24 * 60 * 60 * 1000 } // 24 hours
